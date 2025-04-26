bf045646d6f44ee3cd446fefc1062604eba113e0
Shubham Dambal <shubhamdambal@gmail.com>
2025-04-26 17:44:56 +0530

    Updation

---
+++ mern/client/src/config/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuGrHvsa3_w0FMHL56yfhxAH1-wwfEVwc",
+  authDomain: "foodshareapp-7dc1f.firebaseapp.com",
+  databaseURL: "https://foodshareapp-7dc1f-default-rtdb.firebaseio.com",
+  projectId: "foodshareapp-7dc1f",
+  storageBucket: "foodshareapp-7dc1f.firebasestorage.app",
+  messagingSenderId: "99558777810",
+  appId: "1:99558777810:web:5f3c860940f3cd61b1de0b"
+};
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
