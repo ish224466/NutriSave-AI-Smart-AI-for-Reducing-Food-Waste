df8322ccaf60ea94aaec4983858a4ae9b50f9795
ish224466 <ishwarraut2004@gmail.com>
2025-05-17 22:39:50 +0530

    Removed old folders mern and ss

--- mern/client/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './config/firebase-config.js'
-import App from './App.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-)
